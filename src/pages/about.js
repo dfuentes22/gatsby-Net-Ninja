e19806b67ea2c99b98b712d0737e3665eebf4b00
dfuentes22 <plainpython@gmail.com>
2021-10-11 09:42:28 -0400

    pages and routes

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quibusdam
+        eum nihil ipsum a dolores minus veniam aspernatur modi! Explicabo dolor,
+        accusamus libero maxime laborum asperiores sit illo quos cupiditate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum
+        officia iste, debitis dolorum sint nemo sunt commodi quaerat voluptas
+        repellendus recusandae architecto accusantium atque nostrum eligendi
+        dolor culpa temporibus.
+      </p>
+    </div>
+  )
+}
