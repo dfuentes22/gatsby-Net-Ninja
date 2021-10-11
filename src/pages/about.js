630192aa52b3f5938e1f743035eadc666badc008
dfuentes22 <plainpython@gmail.com>
2021-10-11 10:24:08 -0400

    layout compononent

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,24 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quibusdam
-        eum nihil ipsum a dolores minus veniam aspernatur modi! Explicabo dolor,
-        accusamus libero maxime laborum asperiores sit illo quos cupiditate.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum
-        officia iste, debitis dolorum sint nemo sunt commodi quaerat voluptas
-        repellendus recusandae architecto accusantium atque nostrum eligendi
-        dolor culpa temporibus.
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+          quibusdam eum nihil ipsum a dolores minus veniam aspernatur modi!
+          Explicabo dolor, accusamus libero maxime laborum asperiores sit illo
+          quos cupiditate.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum
+          officia iste, debitis dolorum sint nemo sunt commodi quaerat voluptas
+          repellendus recusandae architecto accusantium atque nostrum eligendi
+          dolor culpa temporibus.
+        </p>
+      </div>
+    </Layout>
   )
 }
