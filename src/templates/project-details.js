f124871e415b63fa508942b3ecd4f38f7f070952
dfuentes22 <plainpython@gmail.com>
2021-10-15 17:14:43 -0400

    add featured image to page details page

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -5,48 +5,46 @@ import { graphql } from "gatsby"
 import * as styles from "../styles/project-details.module.css"
 
 export default function ProjectDetails({ data }) {
+  const featuredImg = getImage(
+    data.markdownRemark.frontmatter.featuredImg.childImageSharp.gatsbyImageData
+  )
+  const { html } = data.markdownRemark
+  const { title, stack } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <GatsbyImage
-            image={getImage(project.frontmatter.featuredImg)}
-            alt={project.frontmatter.title}
-          /> */}
+          <GatsbyImage image={featuredImg} alt={title} />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={project.html} /> */}
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
 
-// export const query = graphql`
-//   query DetailsPage {
-//     projects: allMarkdownRemark(
-//       sort: { fields: frontmatter___date, order: DESC }
-//     ) {
-//       nodes {
-//         frontmatter {
-//           slug
-//           stack
-//           title
-//           date
-//           featuredImg {
-//             childImageSharp {
-//               gatsbyImageData
-//             }
-//           }
-//         }
-//         id
-//         html
-//       }
-//     }
-//     contact: site {
-//       siteMetadata {
-//         contact
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+              layout: FULL_WIDTH
+              placeholder: BLURRED
+              formats: WEBP
+            )
+          }
+        }
+      }
+    }
+  }
+`
